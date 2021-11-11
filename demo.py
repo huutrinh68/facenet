1e2cf1df5c96860dfc0a8d664bdbb7bbd3dc3b12
huutrinh68 <trinhsp89@gmail.com>
2021-11-11 09:50:26 +0900

    add database

--- demo.py
+++ demo.py
@@ -1,8 +1,56 @@
+import os
 from facenet_pytorch import MTCNN, InceptionResnetV1
 from PIL import Image
 import numpy as np
 import torch
 import cv2
+import json
+
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+print('Running on device: {}'.format(device))
+# mtcnn = MTCNN(image_size=160, margin=10)
+mtcnn = MTCNN(image_size=160, margin=0, min_face_size=20,
+              thresholds=[0.6, 0.7, 0.7], factor=0.709, post_process=True,
+              device=device
+              )
+resnet = InceptionResnetV1(pretrained='vggface2').eval()
+
+
+def create_database(databank):
+    database = dict()
+    database['database_path'] = databank
+    database_list = list()
+
+    for name in os.path.listdir(databank):
+        if not os.path.isdir(name):
+            continue
+
+        obj = dict()
+        attribute_list = list()
+        for image_id in os.path.listdir(os.path.join(databank, name)):
+            if not os.path.isfile(image):
+                continue
+            attribute = dict()
+
+            attribute['path'] = os.path.join(databank, name, image_id)
+            image = Image.open(os.path.join(databank, name, image_id))
+            image_cropped = mtcnn(image)
+            embedding = resnet(image_cropped.unsqueeze(0))
+            embedding = embedding.detach().cpu().flatten()
+            attribute['emb'] = embedding
+            attribute_list.append(attribute)
+
+        if len(attribute_list):
+            obj['name'] = name
+            obj['atttribute'] = attribute_list
+            obj['count'] = 1
+    database['database'] = database_list
+
+    with open('./tests/database.json', 'w') as f:
+        json.dump(database, f, indent=4)
+
+    return database
 
 
 def cos_similarity(p1, p2):
@@ -11,37 +59,51 @@ def cos_similarity(p1, p2):
 
 def main():
     cosin = torch.nn.CosineSimilarity(dim=2, eps=1e-6)
-    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-    print('Running on device: {}'.format(device))
-    # mtcnn = MTCNN(image_size=160, margin=10)
-    mtcnn = MTCNN(image_size=160, margin=0, min_face_size=20,
-                  thresholds=[0.6, 0.7, 0.7], factor=0.709, post_process=True,
-                  device=device
-                  )
-    resnet = InceptionResnetV1(pretrained='vggface2').eval()
 
-    image_path1 = "./databank/employee/1.jpg"
-    image_path2 = "./databank/obama/1.jpg"
-    image_path3 = "./databank/trump/1.jpg"
-    dataset = {'employee': image_path1, 'obama': image_path2, 'trump': image_path3}
-    names = list(dataset.keys())
+    # load if exist
+    if os.path.exists('./tests/database.json'):
+        database = json.load('./tests/database.json')
+    else:
+        database = create_database()
 
-    img1 = Image.open(image_path1)
-    img_cropped1 = mtcnn(img1)
-    img_embedding1 = resnet(img_cropped1.unsqueeze(0))
+    database_list = database.get('database', [])
+    names = list()
+    embeddings = list()
+    for obj in database_list:
+        name = obj.get('name', None)
+        attribute = obj.get('attribute', [])
+        count = obj.get('count', 1)
 
-    img2 = Image.open(image_path2)
-    img_cropped2 = mtcnn(img2)
-    img_embedding2 = resnet(img_cropped2.unsqueeze(0))
+        if name is not None and len(attribute):
+            for attr in attribute:
+                embedding = attr.get('emb', None)
+                if embedding is not None:
+                    names.append(name)
+                    embeddings.append(embedding)
 
-    img3 = Image.open(image_path3)
-    img_cropped3 = mtcnn(img3)
-    img_embedding3 = resnet(img_cropped3.unsqueeze(0))
 
-    embeddings = []
-    embeddings.append(img_embedding1.detach().cpu().flatten())
-    embeddings.append(img_embedding2.detach().cpu().flatten())
-    embeddings.append(img_embedding3.detach().cpu().flatten())
+    # image_path1 = "./databank/employee/1.jpg"
+    # image_path2 = "./databank/obama/1.jpg"
+    # image_path3 = "./databank/trump/1.jpg"
+    # dataset = {'employee': image_path1, 'obama': image_path2, 'trump': image_path3}
+    # names = list(dataset.keys())
+
+    # img1 = Image.open(image_path1)
+    # img_cropped1 = mtcnn(img1)
+    # img_embedding1 = resnet(img_cropped1.unsqueeze(0))
+
+    # img2 = Image.open(image_path2)
+    # img_cropped2 = mtcnn(img2)
+    # img_embedding2 = resnet(img_cropped2.unsqueeze(0))
+
+    # img3 = Image.open(image_path3)
+    # img_cropped3 = mtcnn(img3)
+    # img_embedding3 = resnet(img_cropped3.unsqueeze(0))
+
+    # embeddings = []
+    # embeddings.append(img_embedding1.detach().cpu().flatten())
+    # embeddings.append(img_embedding2.detach().cpu().flatten())
+    # embeddings.append(img_embedding3.detach().cpu().flatten())
 
     embeddings = np.stack(embeddings)
     embeddings = torch.from_numpy(embeddings)
@@ -106,7 +168,7 @@ def main():
                 cv2.putText(origin_image, names[best_match_index], (x, y - 4), font, 0.75, (0, 255, 0), 1)
 
         writer.write(origin_image)
-        cv2.imshow('Frame', origin_image)
+        # cv2.imshow('Frame', origin_image)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
